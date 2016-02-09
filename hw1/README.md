28d8213cb61bc92565be188a98e60f0ae86b2b64
Anirudhan J. Rajagopalan <ajr619@nyu.edu>
2016-02-09 12:01:25 -0500

    homework first commit

---
+++ hw1/README.md
@@ -0,0 +1,2 @@
+# ds-ga-1008-a1
+Assignment 1 sample code. Adapted from Clement Farabet’s tutorial on supervised learning.
---
+++ hw1/result.lua
@@ -0,0 +1,8 @@
+require 'torch'   -- torch
+require 'image'   -- for color transforms
+require 'nn'      -- provides a normalization operator
+
+model_path = "."
+test_path = "."
+
+
