e256457c4a15a9fe6dba3bdf135b1b29146cdecc
Anirudhan J. Rajagopalan <ajr619@nyu.edu>
2016-05-03 18:22:02 -0400

    main_gru.lua

---
+++ hw4/main_gru.lua
@@ -0,0 +1,359 @@
+--
+----  Copyright (c) 2014, Facebook, Inc.
+----  All rights reserved.
+----
+
+----  This source code is licensed under the Apache 2 license found in the
+----  LICENSE file in the root directory of this source tree. 
+----
+
+previousAccuracies = {[1]=math.huge, [2]=math.huge, [3]=math.huge, [4]=math.huge, [5]=math.huge}
+
+gpu = false
+if gpu then
+    require 'cunn'
+    print("Running on GPU") 
+    
+else
+    require 'nn'
+    print("Running on CPU")
+end
+
+require('nngraph')
+require('base')
+require('optim')
+ptb = require('data')
+filename_prefix = 'seq_40'
+
+-- Trains 1 epoch and gives validation set ~182 perplexity (CPU).
+local params = {
+                batch_size=20, -- minibatch
+                seq_length=20, -- unroll length
+                layers=2,
+                decay=2,
+                rnn_size=200, -- hidden unit size
+                dropout=0, 
+                init_weight=0.1, -- random weight initialization limits
+                lr=1, --learning rate
+                vocab_size=10000, -- limit on the vocabulary size
+                max_epoch=4,  -- when to start decaying learning rate
+                max_max_epoch=13, -- final epoch
+                max_grad_norm=5 -- clip when gradients exceed this norm value
+               }
+
+function transfer_data(x)
+    if gpu then
+        return x:cuda()
+    else
+        return x
+    end
+end
+
+model = {}
+
+function grucell(input, prevh)
+    local i2h = nn.Linear(params.rnn_size, 3 * params.rnn_size)(input)
+    local h2h = nn.Linear(params.rnn_size, 3 * params.rnn_size)(prevh)
+    local gates = nn.CAddTable()({
+       nn.Narrow(2, 1, 2 * params.rnn_size)(i2h),
+       nn.Narrow(2, 1, 2 * params.rnn_size)(h2h),
+    })
+    gates = nn.SplitTable(2)(nn.Reshape(2, params.rnn_size)(gates))
+    local resetgate = nn.Sigmoid()(nn.SelectTable(1)(gates))
+    local updategate = nn.Sigmoid()(nn.SelectTable(2)(gates))
+    local output = nn.Tanh()(nn.CAddTable()({
+       nn.Narrow(2, 2 * params.rnn_size+1, params.rnn_size)(i2h),
+       nn.CMulTable()({resetgate,
+       nn.Narrow(2, 2 * params.rnn_size+1, params.rnn_size)(h2h),})
+    }))
+    local nexth = nn.CAddTable()({ prevh,
+       nn.CMulTable()({ updategate,
+       nn.CSubTable()({output, prevh,}),}),
+    })
+    return nexth
+end
+
+function create_network()
+    local x                  = nn.Identity()()
+    local y                  = nn.Identity()()
+    local prev_s             = nn.Identity()()
+    local i                  = {[0] = nn.LookupTable(params.vocab_size,
+                                                    params.rnn_size)(x)}
+    local next_s             = {}
+    local split              = {prev_s:split(params.layers)}
+    for layer_idx = 1, params.layers do
+        local prev_h         = split[ layer_idx]
+        local dropped        = nn.Dropout(params.dropout)(i[layer_idx - 1])
+        local next_h = grucell(dropped, prev_h)
+        table.insert(next_s, next_h)
+        i[layer_idx] = next_h
+    end
+    local h2y                = nn.Linear(params.rnn_size, params.vocab_size)
+    local dropped            = nn.Dropout(params.dropout)(i[params.layers])
+    local pred               = nn.LogSoftMax()(h2y(dropped))
+    local err                = nn.ClassNLLCriterion()({pred, y})
+    local module             = nn.gModule({x, y, prev_s},
+                                      {err, nn.Identity()(next_s),pred})
+    -- initialize weights
+    module:getParameters():uniform(-params.init_weight, params.init_weight)
+    return transfer_data(module)
+end
+
+function setup()
+    print("Creating a RNN LSTM network.")
+    local core_network = create_network()
+    paramx, paramdx = core_network:getParameters()
+    model.s = {}
+    model.ds = {}
+    model.start_s = {}
+    for j = 0, params.seq_length do
+        model.s[j] = {}
+        for d = 1,  params.layers do
+            model.s[j][d] = transfer_data(torch.zeros(params.batch_size, params.rnn_size))
+        end
+    end
+    for d = 1,  params.layers do
+        model.start_s[d] = transfer_data(torch.zeros(params.batch_size, params.rnn_size))
+        model.ds[d] = transfer_data(torch.zeros(params.batch_size, params.rnn_size))
+    end
+    model.core_network = core_network
+    model.rnns = g_cloneManyTimes(core_network, params.seq_length)
+    model.norm_dw = 0
+    model.err = transfer_data(torch.zeros(params.seq_length))
+    model.params=params
+end
+
+
+function reset_state(state)
+    state.pos = 1
+    if model ~= nil and model.start_s ~= nil then
+        for d = 1, params.layers do
+            model.start_s[d]:zero()
+        end
+    end
+end
+
+function reset_ds()
+    for d = 1, #model.ds do
+        model.ds[d]:zero()
+    end
+end
+
+function fp(state)
+    -- g_replace_table(from, to).  
+    g_replace_table(model.s[0], model.start_s)
+    
+    -- reset state when we are done with one full epoch
+    if state.pos + params.seq_length > state.data:size(1) then
+        reset_state(state)
+    end
+    
+    -- forward prop
+    for i = 1, params.seq_length do
+        local x = state.data[state.pos]
+        local y = state.data[state.pos + 1]
+        local s = model.s[i - 1]
+        model.err[i], model.s[i],preds = unpack(model.rnns[i]:forward({x, y, s}))
+        local val,ind = torch.max(preds,2)
+        state.pos = state.pos + 1
+    end
+    
+    -- next-forward-prop start state is current-forward-prop's last state
+    g_replace_table(model.start_s, model.s[params.seq_length])
+    
+    -- cross entropy error
+    return model.err:mean()
+end
+
+function bp(state)
+    -- start on a clean slate. Backprop over time for params.seq_length.
+    paramdx:zero()
+    reset_ds()
+    for i = params.seq_length, 1, -1 do
+        -- to make the following code look almost like fp
+        state.pos = state.pos - 1
+        local x = state.data[state.pos]
+        local y = state.data[state.pos + 1]
+        local s = model.s[i - 1]
+        -- Why 1?
+        local derr = transfer_data(torch.ones(1))
+        -- tmp stores the ds
+        local tmp = model.rnns[i]:backward({x, y, s},
+                                           {derr, model.ds,torch.zeros(preds:size())})[3]
+        -- remember (to, from)
+        g_replace_table(model.ds, tmp)
+    end
+    
+    -- undo changes due to changing position in bp
+    state.pos = state.pos + params.seq_length
+    
+    -- gradient clipping
+    model.norm_dw = paramdx:norm()
+    if model.norm_dw > params.max_grad_norm then
+        local shrink_factor = params.max_grad_norm / model.norm_dw
+        paramdx:mul(shrink_factor)
+    end
+    
+    -- gradient descent step
+    paramx:add(paramdx:mul(-params.lr))
+end
+
+-- prevAccuracy is an increasingly sorted list of top five accuracy of the models
+function shouldSave(curAccu, prevAccu)
+  if(curAccu > prevAccu[1]) then
+    --print("first return")
+    return false
+  end
+
+  local retVal = false
+  local found = 5
+  local todel = 0
+
+  for idx=1,5,1 do
+    if curAccu > prevAccu[idx] then
+      found = idx - 1
+      --print("break")
+      break
+    end
+  end
+
+  --print("found is " .. found)
+
+  if found >= 1 and prevAccu[found] ~= curAccu then 
+    todel = prevAccu[1]
+    swapElements(prevAccu, found)
+    prevAccu[found] = curAccu
+    retVal = true
+    --print("true break")
+  end
+    return retVal, todel
+end
+
+
+function run_valid()
+    -- again start with a clean slate
+    reset_state(state_valid)
+    
+    -- no dropout in testing/validating
+    g_disable_dropout(model.rnns)
+    
+    -- collect perplexity over the whole validation set
+    local len = (state_valid.data:size(1) - 1) / (params.seq_length)
+    local perp = 0
+    for i = 1, len do
+        perp = perp + fp(state_valid)
+    end
+    print("Validation set perplexity : " .. g_f3(torch.exp(perp / len)))
+    g_enable_dropout(model.rnns)
+
+
+    perplexityToSave = g_f3(torch.exp(perp / len))
+    -- save model
+    --model:get(3) sostituito con model???
+    save_model, model_to_remove = shouldSave(perplexityToSave, previousAccuracies)
+    if save_model then
+      local filename = paths.concat('logsGru', 'model.net' .. perplexityToSave)
+      print('==> saving model to '..filename)
+      torch.save(filename, model)
+      local fileToRemove = paths.concat('logsGru', 'model.net' .. model_to_remove)
+      os.remove(fileToRemove)
+    end
+
+
+
+end
+
+function run_test()
+    reset_state(state_test)
+    g_disable_dropout(model.rnns)
+    local perp = 0
+    local len = state_test.data:size(1)
+    
+    -- no batching here
+    g_replace_table(model.s[0], model.start_s)
+    for i = 1, (len - 1) do
+        local x = state_test.data[i]
+        local y = state_test.data[i + 1]
+        perp_tmp, model.s[1] = unpack(model.rnns[1]:forward({x, y, model.s[0]}))
+        perp = perp + perp_tmp[1]
+        g_replace_table(model.s[0], model.s[1])
+    end
+    print("Test set perplexity : " .. g_f3(torch.exp(perp / (len - 1))))
+    g_enable_dropout(model.rnns)
+end
+
+if gpu then
+    g_init_gpu(arg)
+end
+
+-- get data in batches
+state_train = {data=transfer_data(ptb.traindataset(params.batch_size))}
+state_valid =  {data=transfer_data(ptb.validdataset(params.batch_size))}
+state_test =  {data=transfer_data(ptb.testdataset(params.batch_size))}
+
+print("Network parameters:")
+print(params)
+
+logger = optim.Logger('logs/'..filename_prefix..'_train.log')
+
+local states = {state_train, state_valid, state_test}
+for _, state in pairs(states) do
+    reset_state(state)
+end
+setup()
+step = 0
+epoch = 0
+total_cases = 0
+beginning_time = torch.tic()
+start_time = torch.tic()
+print("Starting training.")
+words_per_step = params.seq_length * params.batch_size
+epoch_size = torch.floor(state_train.data:size(1) / params.seq_length)
+
+while epoch < params.max_max_epoch do
+
+    -- take one step forward
+    perp = fp(state_train)
+    if perps == nil then
+        perps = torch.zeros(epoch_size):add(perp)
+    end
+    perps[step % epoch_size + 1] = perp
+    step = step + 1
+    
+    -- gradient over the step
+    bp(state_train)
+    
+    -- words_per_step covered in one step
+    total_cases = total_cases + params.seq_length * params.batch_size
+    epoch = step / epoch_size
+    
+    -- display details at some interval
+    if step % torch.round(epoch_size / 10) == 10 then
+        wps = torch.floor(total_cases / torch.toc(start_time))
+        since_beginning = g_d(torch.toc(beginning_time) / 60)
+        print( 'step = '.. g_f3(step) .. ', epoch = ' .. g_f3(epoch) ..
+             ', train perp. = ' .. g_f3(torch.exp(perps:mean())) ..
+             ', wps = ' .. wps ..
+             ', dw:norm() = ' .. g_f3(model.norm_dw) ..
+             ', lr = ' ..  g_f3(params.lr) ..
+             ', since beginning = ' .. since_beginning .. ' mins.')
+        logger:add{['step'] = train_error,
+                  ['epoch'] = test_error,
+                  ['train perp'] = g_f3(torch.exp(perps:mean())),
+                  ['wps'] = wps,
+                  ['dwnorm'] = g_f3(model.norm_dw),
+                  ['lr'] = g_f3(params.lr),
+                  ['numMins'] = since_beginning
+                  }
+    end
+    
+    -- run when epoch done
+    if step % epoch_size == 0 then
+        run_valid()
+        if epoch > params.max_epoch then
+            params.lr = params.lr / params.decay
+        end
+    end
+end
+run_test()
+print("Training is over.")
