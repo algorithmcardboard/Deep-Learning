6560d223dcd41969423e4c165e2f996156e448e4
Anirudhan J. Rajagopalan <ajr619@nyu.edu>
2016-05-03 16:04:57 -0400

    final query_sentences.lua

--- hw4/query_sentences.lua
+++ hw4/query_sentences.lua
@@ -2,44 +2,19 @@ require 'base';
 require 'nn';
 require 'nngraph';
 require 'torch';
-stringx = require('pl.stringx')
 require 'io'
+stringx = require('pl.stringx')
 ptb = require('data')
 
-params = {}
-params.rnn_size = 200
-params.layers = 2
-params.gru = false
-params.seq_length = 20
-
-model = {}
-model.s = {}
-model.s[0] = {}
-for d = 1, 2 * params.layers do
-  model.s[0][d] = torch.zeros(1, params.rnn_size)
-end
-
---print(model.s)
-
-model_file = "/scratch/ajr619/lstm/model.net120.10116158295"
-print ("Loading model "..model_file)
-m = torch.load(model_file)
-model.core_network = m.core_network
-
-print ("Building vocab and initialize states")
-local trainfn = "data/ptb.train.txt"
-local testfn  = "data/ptb.test.txt"
-local validfn = "data/ptb.valid.txt"
-
-ptb.load_data(trainfn)
-ptb.load_data(validfn)
-ptb.load_data(testfn)
 
 function readline()
   local line = io.read("*line")
-  if line == nil then error({code="EOF"}) end
+
+  if line == nil or #line == 0 then error({code="EOF"}) end
   line = stringx.split(line)
+  if line == nil then error({code="EOF"}) end
   if tonumber(line[1]) == nil then error({code="init"}) end
+
   for i = 2,#line do
     if ptb.vocab_map[line[i]] == nil then line[i] = '<unk>' end
   end
@@ -51,9 +26,8 @@ function getline()
   local ok, line = pcall(readline)
   if not ok then
     if line.code == "EOF" then
+        print ('code is EOF')
         return line.code
-    elseif line.code == "vocab" then
-      print("Word not in vocabulary, only 'foo' is in vocabulary: ", line.word)
     elseif line.code == "init" then
       print("Start with a number")
     else
@@ -65,51 +39,68 @@ function getline()
   end
 end
 
+
+function get_model()
+  model = {
+    s = {
+      [0] = {}
+    }
+  }
+  for d = 1, 2 * params.layers do
+    model.s[0][d] = torch.zeros(1, params.rnn_size)
+  end
+
+
+  model.core_network = torch.load(MODEL_FILE).core_network
+  g_disable_dropout(model.core_network)
+
+  return model
+end
+
+
+function get_vocab_inverse_map()
+  local trainfn = "data/ptb.train.txt"
+  local testfn  = "data/ptb.test.txt"
+  local validfn = "data/ptb.valid.txt"
+  ptb.load_data(trainfn)
+  ptb.load_data(validfn)
+  ptb.load_data(testfn)
+
+  return ptb.vocab_map, ptb.inverse_map
+end
+
+
 function get_next_words(line)
   local n_words = tonumber(line[1])
-  -- Fillup hidden states with known words
-  local x
-  local y
-  x = torch.Tensor(1):fill(ptb.vocab_map[line[#line - 1]])
-  y = torch.Tensor(1):fill(ptb.vocab_map[line[#line]])
+  -- initialize x and y to the penultimate and last words
+  local x = torch.Tensor(1):fill(ptb.vocab_map[line[#line - 1]])
+  local y = torch.Tensor(1):fill(ptb.vocab_map[line[#line]])
+
   for i = 2, #line-3 do
     x = torch.Tensor(1):fill(ptb.vocab_map[line[i]])
     y = torch.Tensor(1):fill(ptb.vocab_map[line[i + 1]])
 
-    -- print ('x is ', x)
-    -- print ('y is ', y)
-    
-    output = model.core_network:forward({x, y, model.s[0]})
-    -- print (output)
-    perp_temp, model.s[1] = unpack(output)
+    perp_temp, model.s[1] = unpack(model.core_network:forward({x, y, model.s[0]}))
     g_replace_table(model.s[0], model.s[1])
   end
-  -- make model generate more
-  -- local x = torch.Tensor(1):fill(ptb.vocab_map[line[#line]])
-  -- local y = torch.Tensor(1):fill(ptb.vocab_map[line[#line]])
+
   local next_words = {}
-  -- print(x)
-  -- print(y)
   for i = 1, n_words  do 
-    -- doesn't matter what y is since we're just generating predictions.
-    -- we just need the x's and s's
     perp_temp, model.s[1] = unpack(model.core_network:forward({x, y, model.s[0]}))
     
-    local logsoftmax = nil
-    local word_ind = nil
-    --logsoftmax, word_ind = model.core_network.modules[44].output:max(2)
-    word_ind = torch.multinomial(torch.exp(model.core_network.modules[44].output), 1)
-    local next_word = ptb.inverse_map[word_ind[1][1]]
-    -- print('word index is ', word_ind, ' word is ', next_word)
-    next_words[i] = next_word
+    local word_ind = torch.multinomial(torch.exp(model.core_network.modules[44].output), 1)
+    local word = ptb.inverse_map[word_ind[1][1]]
+    next_words[i] = word
+
+    -- swap
     x = y
     y = torch.Tensor(1):fill(ptb.vocab_map[next_word])
+
     g_replace_table(model.s[0], model.s[1])
-    
   end
+
   return next_words
 end
-
 
 function query_sentences()
   while true do
@@ -128,12 +119,20 @@ function query_sentences()
   end
 end
 
-function disable_dropout(node)
-  if string.match(node.__typename, "Dropout") then
-    node.train = false
-  end
-end
+params = {
+  rnn_size = 200,
+  layers = 2,
+  gru = false,
+  seq_length = 20
+}
 
-g_disable_dropout(model.core_network)
+print ("Loading model "..MODEL_FILE)
 
+MODEL_FILE = "/scratch/ajr619/lstm/model.net120.10116158295"
+model = get_model()
+
+print("Building vocab and initialize states")
+vocab_map, inverse_map = get_vocab_inverse_map()
+
+-- Querying sentences
 query_sentences()
